919500f4a7c87e9ad80bf1f82aa74ff2e48e728f
Aadarsh-Keshri <keshri.aadarsh123@gmail.com>
2026-03-19 01:27:39 +0530

    [Support][Modules] Ensure parent directory exists in LockFileManager

--- llvm/lib/Support/LockFileManager.cpp
+++ llvm/lib/Support/LockFileManager.cpp
@@ -16,6 +16,7 @@
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/IOSandbox.h"
 #include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/Path.h"
 #include "llvm/Support/Process.h"
 #include "llvm/Support/Signals.h"
 #include "llvm/Support/raw_ostream.h"
@@ -171,6 +172,13 @@ Expected<bool> LockFileManager::tryLock() {
   if (std::error_code EC = sys::fs::make_absolute(AbsoluteFileName))
     return createStringError(EC, "failed to obtain absolute path for " +
                                      AbsoluteFileName);
+
+  SmallString<128> Dir = sys::path::parent_path(AbsoluteFileName);
+  if (!Dir.empty()) {
+    if (std::error_code EC = sys::fs::create_directories(Dir))
+      return createStringError(EC, "failed to create lock directory " + Dir);
+  }
+
   LockFileName = AbsoluteFileName;
   LockFileName += ".lock";
 
